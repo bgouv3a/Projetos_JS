1d2d7b4440532b813afde17ae31d4117af6f33bd
bgouv3a <brayan.gouvearocha@gmail.com>
2026-03-12 20:31:14 -0300

    imc

---
+++ 03_IMC/03.js
@@ -0,0 +1,34 @@
+
+const btnCalcular = document.getElementById("calcular");
+const res = document.getElementById("resultado")
+
+
+btnCalcular.addEventListener("click",()=>{
+    const peso = Number(document.getElementById("escrevaPeso").value);
+    const altura = Number(document.getElementById("escrevaAltura").value)
+    let imc = peso / (altura * altura);
+
+    if (imc<18.5) {
+        res.innerText = (`A sua classificação IMC é: Baixo Peso`)
+        
+    } else if (imc <25){
+        res.innerText = (`A sua classificação IMC é: Peso Normal`)
+    }
+    else if (imc <30){
+       res.innerText = (`A sua classificação IMC é: Sobrepeso`)
+    }
+    else if (imc <35){
+        res.innerText = (`A sua classificação IMC é: Obesidade 1`)
+        
+    }
+    else if (imc <40){
+        res.innerText = (`A sua classificação IMC é: Obesidade 2`)
+    }
+    else if (imc >=40){
+        res.innerText = (`A sua classificação IMC é: Obesidade Mórbida`)
+        
+    }
+
+
+
+})
