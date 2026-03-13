3dfc37892df12a0a3e7d9e4992ee42bccd54a7c6
bgouv3a <brayan.gouvearocha@gmail.com>
2026-03-12 23:04:03 -0300

    aa

--- 03_IMC/03.js
+++ 03_IMC/03.js
@@ -1,31 +1,32 @@
 
 const btnCalcular = document.getElementById("calcular");
-const res = document.getElementById("resultado")
+const res = document.getElementById("resultado");
 
 
 btnCalcular.addEventListener("click",()=>{
     const peso = Number(document.getElementById("escrevaPeso").value);
-    const altura = Number(document.getElementById("escrevaAltura").value)
+    const altura = Number(document.getElementById("escrevaAltura").value);
     let imc = peso / (altura * altura);
 
+
     if (imc<18.5) {
-        res.innerText = (`A sua classificação IMC é: Baixo Peso`)
+        res.innerHTML = (`A sua classificação IMC é:`);
         
     } else if (imc <25){
-        res.innerText = (`A sua classificação IMC é: Peso Normal`)
+        res.innerText = (`A sua classificação IMC é: Peso Normal`);
     }
     else if (imc <30){
-       res.innerText = (`A sua classificação IMC é: Sobrepeso`)
+       res.innerText = (`A sua classificação IMC é: Sobrepeso`);
     }
     else if (imc <35){
-        res.innerText = (`A sua classificação IMC é: Obesidade 1`)
+        res.innerText = (`A sua classificação IMC é: Obesidade 1`);
         
     }
     else if (imc <40){
-        res.innerText = (`A sua classificação IMC é: Obesidade 2`)
+        res.innerText = (`A sua classificação IMC é: Obesidade 2`);
     }
     else if (imc >=40){
-        res.innerText = (`A sua classificação IMC é: Obesidade Mórbida`)
+        res.innerText = (`A sua classificação IMC é: Obesidade Mórbida`);
         
     }
 
